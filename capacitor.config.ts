b79e852fae335a8983371bcfa6617294aad3151b
CMJuanFernandoDmc <151117518+CMJuanFernandoDmc@users.noreply.github.com>
2024-01-12 10:48:47 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sumativa3CaldasJuan',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
